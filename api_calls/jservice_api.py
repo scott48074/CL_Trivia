2e7f962f4fccb9b8e558e751dcff98f7ea6d588f
Scott Frasier <scottfrasier@Scotts-MacBook-Pro.local>
2020-10-27 20:34:44 -0400

    Clues Api

--- api_calls/jservice_api.py
+++ api_calls/jservice_api.py
@@ -8,7 +8,29 @@ import json
 
 
 # Type hints: returning a list of dictionaries or None in case of an error.
-def random_question(count: int = 1) -> Union[List[Dict[str, str]], None]:
+def clue(**params) -> Union[List[Dict[str, str]], None]:
+    """
+    params:
+    value(int): the value of the clue in dollars
+    category(int): the id of the category you want to return
+    min_date(date): earliest date to show, based on original air date
+    max_date(date): latest date to show, based on original air date
+    offset(int): offsets the returned clues. Useful in pagination
+    """
+    try:
+        if params:
+            params = {k: v for (k, v) in zip(params.keys(), params.items())}
+
+        r = requests.get('http://jservice.io/api/clues', params=params)
+        r.raise_for_status()
+        return json.loads(r.text)
+    except HTTPError as e:
+        print(f'HTTP error: {e}')
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+def random_clue(count: int = 1) -> Union[List[Dict[str, str]], None]:
     # Count is the number of questions to return (100 max).
     try:
         params = {'count': str(count)}
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,22 +4,41 @@ from api_calls import jservice_api
 
 
 def test_random_question_api_status():
-    random_questions = jservice_api.random_question()[0]
+    random_clue = jservice_api.random_clue()[0]
     # If an error occurs None will be returned.
     # Use pytest with the [-s] flag to print the errors in the console.
-    assert random_questions
+    assert random_clue
 
 
 def test_random_question_api_shape():
     # Making sure the shape of the API didn't change.
-    random_questions = jservice_api.random_question()[0]
+    random_clue = jservice_api.random_clue()[0]
     expected_keys = ['id', 'answer', 'question', 'value', 'airdate', 'created_at', 'updated_at', 'category_id',
                      'game_id', 'invalid_count', 'category']
-    returned_keys = [key for key in random_questions.keys()]
+    returned_keys = [key for key in random_clue.keys()]
     assert returned_keys == expected_keys
 
 
 def test_random_question_return_multiple_questions():
     # Test that the count param is working as expected..
-    random_questions = jservice_api.random_question(count=10)
-    assert len(random_questions) == 10
+    random_clue = jservice_api.random_clue(count=10)
+    assert len(random_clue) == 10
+
+def test_clue_api():
+    clue = jservice_api.clue()[0]
+    # If an error occurs None will be returned.
+    # Use pytest with the [-s] flag to print the errors in the console.
+    assert clue
+
+def test_clue_api_shape():
+    # Making sure the shape of the API didn't change.
+    clue = jservice_api.clue()[0]
+    expected_keys = ['id', 'answer', 'question', 'value', 'airdate', 'created_at', 'updated_at', 'category_id',
+                     'game_id', 'invalid_count', 'category']
+    returned_keys = [key for key in clue.keys()]
+    assert returned_keys == expected_keys
+
+def test_clue_params():
+    clue = jservice_api.clue(value=800, category=1)[0]
+    assert clue['value'] == 800
+    assert clue['category_id'] == 1
